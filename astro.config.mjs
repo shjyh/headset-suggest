1eb497bf99f2a0ddb67ff9aff3de9cd41fa4d283
wangkehan <shjyh651079151@gmail.com>
2024-03-22 23:59:30 +0800

    init project

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
---
+++ src/pages/t.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>aaaaabb {{ a }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const a = ref(1);
+</script>
